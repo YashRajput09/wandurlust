f20558756476bcc0647aba49228269841fd1ff0c
yash rajput <rajputyash8561@gmail.com>
2024-03-31 17:06:25 +0530

    add marker to the map location

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,13 @@
 mapboxgl.accessToken = mapAccessToken;
 const map = new mapboxgl.Map({
     container: 'map',   //container ID
-    style: 'mapbox://styles/mapbox/streets-v9', //mapbox style
+    style: 'mapbox://styles/mapbox/streets-v12', //mapbox style
     projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
     center: [75.8577, 22.7196], //satrting position [lng, lat]
-    zoom: 9,  //starting zoom
+    zoom: 10,  //starting zoom
 });
+
+console.log(coordinates);
+const marker = new mapboxgl.Marker({color: 'darkred'})
+.setLngLat(coordinates)  //listing.location.coordinates (lat, lng)
+.addTo(map);
