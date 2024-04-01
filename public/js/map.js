c4ec1951f16d9a2b572286d158aa2684ad7df708
yash rajput <rajputyash8561@gmail.com>
2024-04-01 07:38:35 +0530

    click map icon, show popUp

--- public/js/map.js
+++ public/js/map.js
@@ -3,11 +3,15 @@ const map = new mapboxgl.Map({
     container: 'map',   //container ID
     style: 'mapbox://styles/mapbox/streets-v12', //mapbox style
     projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
-    center: [75.8577, 22.7196], //satrting position [lng, lat]
+    center: listing.geometry.coordinates, //satrting position [lng, lat]
     zoom: 10,  //starting zoom
 });
 
-console.log(coordinates);
 const marker = new mapboxgl.Marker({color: 'darkred'})
-.setLngLat(coordinates)  //listing.location.coordinates (lat, lng)
+.setLngLat(listing.geometry.coordinates)  //listing.location.coordinates (lat, lng)
+.setPopup(
+    new mapboxgl.Popup({   offset: 25 }).setHTML(
+        `<h4>${listing.title}</h4><p>Exact location will be provided after booking.</p>`
+    ) // add popups
+)
 .addTo(map);
