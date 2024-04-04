e9654b38c9f41f7628a4d22692b76f78d61f2ed9
yash rajput <rajputyash8561@gmail.com>
2024-04-04 19:24:33 +0530

    write js code from(index, category.ejs) file to script.js file

--- public/js/script.js
+++ public/js/script.js
@@ -18,4 +18,15 @@
     })
   })()
 
- 
+  // tax button click handler
+  let taxSwitch = document.querySelector('#flexSwitchCheckDefault');
+  taxSwitch.addEventListener('click', () =>{
+    let taxInfo = document.getElementsByClassName('tax-info');
+    for(info of taxInfo){
+    if (info.style.display != 'inline') {
+      info.style.display = 'inline';
+    } else{
+      info.style.display = 'none';
+    }
+  }
+  })
