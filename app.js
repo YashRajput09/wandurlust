900035665a4b7fb6cea3e9a02390979a78d119fd
Yash Rajput <rajputyash8561@gmail.com>
2024-06-10 19:57:19 +0530

    custom error when path is not match with any route

--- app.js
+++ app.js
@@ -96,9 +96,9 @@ app.use('/user', userRoute);
 
 
 // if request is not match to any route the this  will execute
-// app.use("*", (req, res, next) => {
-//   next(new ExpressError(404, "Page Not Found"));
-// });
+app.use("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
 
 // error handling middleware
 // deconstruct the err and send res.
